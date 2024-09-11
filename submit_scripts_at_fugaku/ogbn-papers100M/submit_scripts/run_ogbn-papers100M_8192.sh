3a742871c3699c80afa79ac0bfcc9436e1812b97
zhuangbility111 <549694389@qq.com>
2024-09-11 23:46:47 +0900

    update running scripts for label propagation.

--- submit_scripts_at_fugaku/ogbn-papers100M/submit_scripts/run_ogbn-papers100M_8192.sh
+++ submit_scripts_at_fugaku/ogbn-papers100M/submit_scripts/run_ogbn-papers100M_8192.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
 #PJM -L "rscunit=rscunit_ft01,rscgrp=large"
-#PJM -L elapse=1:00:00 
+#PJM -L elapse=2:00:00 
 #PJM -g ra000012 
 #PJM -L "node=16x8x16:torus:strict-io,freq=2000"
 #PJM --mpi "proc=8192"
+#PJM --mpi "max-proc-per-node=4"
 #PJM -j
 #PJM -S
 
 source ~/gnn/gnn/pytorch/config_env.sh
 graph_name=ogbn-papers100M
-res_dir=../log/perf
+is_label_augment=false
+res_dir=../log/sage_perf_label_${is_label_augment}
 mkdir -p ${res_dir}
 dir_stdout=${res_dir}/${PJM_MPI_PROC}proc/
 
 tcmalloc_path=/home/ra000012/a04083/gnn/gnn/pytorch/scripts/fujitsu/lib/libtcmalloc.so
 date
-LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD mpirun -np ${PJM_MPI_PROC} -stdout-proc ${dir_stdout}/ori/stdout -stderr-proc ${dir_stdout}/ori/stderr python ../../../examples/graphsage/train.py --config=../../../examples/graphsage/config/fugaku/${graph_name}.yaml --num_bits=32 --is_label_augment=true
-LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD mpirun -np ${PJM_MPI_PROC} -stdout-proc ${dir_stdout}/ori+pre/stdout -stderr-proc ${dir_stdout}/ori+pre/stderr python ../../../examples/graphsage/train.py --config=../../../examples/graphsage/config/fugaku/${graph_name}.yaml --num_bits=32 --is_pre_delay=true --is_label_augment=true
-LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD mpirun -np ${PJM_MPI_PROC} -stdout-proc ${dir_stdout}/ori+int2/stdout -stderr-proc ${dir_stdout}/ori+int2/stderr python ../../../examples/graphsage/train.py --config=../../../examples/graphsage/config/fugaku/${graph_name}.yaml --num_bits=2 --is_label_augment=true
-LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD mpirun -np ${PJM_MPI_PROC} -stdout-proc ${dir_stdout}/ori+pre+int2/stdout -stderr-proc ${dir_stdout}/ori+pre+int2/stderr python ../../../examples/graphsage/train.py --config=../../../examples/graphsage/config/fugaku/${graph_name}.yaml --num_bits=2 --is_pre_delay=true --is_label_augment=true
+LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD mpirun -np ${PJM_MPI_PROC} -stdout-proc ${dir_stdout}/ori/stdout -stderr-proc ${dir_stdout}/ori/stderr python ../../../examples/graphsage/train.py --config=../../../examples/graphsage/config/fugaku/${graph_name}.yaml --num_bits=32 --is_label_augment=${is_label_augment}
+LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD mpirun -np ${PJM_MPI_PROC} -stdout-proc ${dir_stdout}/ori+pre/stdout -stderr-proc ${dir_stdout}/ori+pre/stderr python ../../../examples/graphsage/train.py --config=../../../examples/graphsage/config/fugaku/${graph_name}.yaml --num_bits=32 --is_pre_delay=true --is_label_augment=${is_label_augment}
+LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD mpirun -np ${PJM_MPI_PROC} -stdout-proc ${dir_stdout}/ori+int2/stdout -stderr-proc ${dir_stdout}/ori+int2/stderr python ../../../examples/graphsage/train.py --config=../../../examples/graphsage/config/fugaku/${graph_name}.yaml --num_bits=2 --is_label_augment=${is_label_augment}
+LD_PRELOAD=$tcmalloc_path:$LD_PRELOAD mpirun -np ${PJM_MPI_PROC} -stdout-proc ${dir_stdout}/ori+pre+int2/stdout -stderr-proc ${dir_stdout}/ori+pre+int2/stderr python ../../../examples/graphsage/train.py --config=../../../examples/graphsage/config/fugaku/${graph_name}.yaml --num_bits=2 --is_pre_delay=true --is_label_augment=${is_label_augment}
 date
